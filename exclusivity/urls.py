51526eb086028db75b4d1cef8be21ffbd1cb0083
MILLOGO <nicolasmillogo3@gmail.com>
2023-08-18 11:45:28 +0000

    Gestion des chemin pour les media

--- exclusivity/urls.py
+++ exclusivity/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls.i18n import set_language
+from django.views.static import serve
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -24,8 +25,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("exclusivity.api_urls")),
     path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("i18n/", set_language, name="set_language"),
 ]
 # if settings.DEBUG:
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
