0caa792c4053ac9176fcee4371712650c1008707
MILLOGO <nicolasmillogo3@gmail.com>
2023-08-18 10:48:27 +0000

    Correction whitenoise pour la gestion des media

--- exclusivity/urls.py
+++ exclusivity/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("i18n/", set_language, name="set_language"),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
