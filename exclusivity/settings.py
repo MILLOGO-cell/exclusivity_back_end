1b9c20349e8e39ff3f84c755f2c1457a0e5aed87
MILLOGO <nicolasmillogo3@gmail.com>
2023-08-22 23:57:28 +0000

    Correction des bugs des abonnements

--- exclusivity/settings.py
+++ exclusivity/settings.py
@@ -31,7 +31,7 @@ SWAGGER_SETTINGS = {
 ALLOWED_HOSTS = ["web-production-abe3.up.railway.app"]
 SITE_ID = 1
 CSRF_TRUSTED_ORIGINS = ["https://web-production-abe3.up.railway.app"]
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
 # CORS_ALLOW_CREDENTIALS = True
 # CORS_ALLOWED_ORIGINS = [
 #     "https://web-production-abe3.up.railway.app",
